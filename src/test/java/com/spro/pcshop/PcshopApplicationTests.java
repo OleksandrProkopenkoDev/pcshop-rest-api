82ded591c8d62cc9b5703e280de1360b99fcacac
SPro <prokopenko89@gmail.com>
2023-08-17 15:54:25 +0300

    initial commit

---
+++ src/test/java/com/spro/pcshop/PcshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spro.pcshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PcshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
