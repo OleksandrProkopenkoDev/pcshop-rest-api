f1ccd5f0032bf5826c961e64f5898062f434b3f4
SPro <prokopenko89@gmail.com>
2023-08-17 15:54:48 +0300

    initial commit

--- src/main/java/com/spro/pcshop/PcshopApplication.java
+++ src/main/java/com/spro/pcshop/PcshopApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PcshopApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PcshopApplication.class, args);
+		 SpringApplication.run(PcshopApplication.class, args);
 	}
 
 }
